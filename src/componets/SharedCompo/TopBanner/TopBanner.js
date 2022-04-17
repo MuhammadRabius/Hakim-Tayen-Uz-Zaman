a1f196c00c5043d95fc1f6dfa07bb93bed71c0c5
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-04-17 14:17:59 +0600

    topbanner updated with heroco icon and done with design

--- src/componets/SharedCompo/TopBanner/TopBanner.js
+++ src/componets/SharedCompo/TopBanner/TopBanner.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import { HiMail,HiPhoneOutgoing,HiLocationMarker } from "react-icons/hi";
 
 
 const TopBanner = () => {
       return (
-            <div className='flex justify-end gap-3  p-2 bg-indigo-700'>
-                  <p className='fw-bold text-white'>support.hakimtayen.splab@gmail.com</p>
-                  <p className='fw-bold text-white'>+88 01999-999999</p>
+            <div className='flex justify-center items-center gap-11  p-2 bg-indigo-500'>
+                  <p className='< text-white flex items-center'> <HiMail className='mx-2 text-2xl'/>support.hakimtayen.splab@gmail.com </p>
+                  <p className=' text-white flex items-center gap-2'> <HiPhoneOutgoing className='mx-2 text-2xl'/>Emergency Line: +88 01999-999999</p>
+                  <p className=' text-white flex items-center '> <HiLocationMarker className='mx-2 text-2xl'/> Location: BSSIC Shilpa Nogory Chandpur </p>
             </div>
       );
 };
