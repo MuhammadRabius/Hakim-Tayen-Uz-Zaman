fb3f93b1e7c2f11de5a455fbf851784e2db07342
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-04-17 13:08:58 +0600

    top banner section updated on ui

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './componets/SharedCompo/Header/Header';
 import './App.css';
 import Footer from './componets/SharedCompo/Footer/Footer';
+import TopBanner from './componets/SharedCompo/TopBanner/TopBanner';
 
 
 function App() {
   return (
     <div>
+      <TopBanner></TopBanner>
       <Header></Header>
       <Routes>
         <Route></Route>
---
+++ src/componets/SharedCompo/TopBanner/TopBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const TopBanner = () => {
+      return (
+            <div className='flex justify-end gap-3  p-2 bg-indigo-700'>
+                  <p className='fw-bold text-white'>support.hakimtayen.splab@gmail.com</p>
+                  <p className='fw-bold text-white'>+88 01999-999999</p>
+            </div>
+      );
+};
+
+export default TopBanner;
